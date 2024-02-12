f9676b3a84c2d7b366f9e7dc078cf668952eb518
Ebrahim Abd EL Razik <ebrahimabdelrazik2002@gmail.com>
2024-02-12 05:38:03 +0200

    Refactor cart controller and routes

--- controller/cart.js
+++ controller/cart.js
@@ -15,4 +15,99 @@ const cart = async (req, res) => {
   }
 };
 
-module.exports = cart;
+const addToCart = async (req, res) => {
+  const userId = req.user._id;
+  const asin = req.body.asin;
+  const quantity = req.body.quantity || 1;
+
+  try {
+    let cart = await Cart.findOne({ userId });
+    if (!cart) {
+      cart = new Cart({ userId, items: [] });
+    }
+
+    const existingItem = cart.items.find((item) => item.asin === asin);
+    if (existingItem) {
+      existingItem.quantity += quantity;
+    } else {
+      cart.items.push({ asin, quantity });
+    }
+
+    await cart.save();
+    res.json(cart);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+};
+
+const clearCart = async (req, res) => {
+  const userId = req.user._id;
+
+  try {
+    await Cart.findOneAndDelete({ userId });
+    res.status(204).send(); // No Content
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+};
+
+const deleteFromCart = async (req, res) => {
+  const userId = req.user._id;
+  const asinToDelete = req.params.asin;
+
+  try {
+    let cart = await Cart.findOne({ userId });
+    if (!cart) {
+      return res.status(404).json({ error: "Cart not found" });
+    }
+
+    cart.items = cart.items.filter((item) => item.asin !== asinToDelete);
+    await cart.save();
+    res.json(cart);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+};
+
+const decrementQuantity = async (req, res) => {
+  const userId = req.user._id;
+  const asinToUpdate = req.params.asin;
+  const quantityToSubtract = req.body.quantity || 1;
+
+  try {
+    let cart = await Cart.findOne({ userId });
+    if (!cart) {
+      return res.status(404).json({ error: "Cart not found" });
+    }
+
+    const existingItem = cart.items.find((item) => item.asin === asinToUpdate);
+    if (!existingItem) {
+      return res.status(404).json({ error: "Product not found in cart" });
+    }
+
+    if (existingItem.quantity <= quantityToSubtract) {
+      // If the quantity to subtract is greater than or equal to the existing quantity, remove the item from the cart
+      cart.items = cart.items.filter((item) => item.asin !== asinToUpdate);
+    } else {
+      // Otherwise, decrement the quantity
+      existingItem.quantity -= quantityToSubtract;
+    }
+
+    await cart.save();
+    res.json(cart);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+};
+
+module.exports = {
+  cart,
+  addToCart,
+  deleteFromCart,
+  clearCart,
+  decrementQuantity,
+};
--- routes/route.js
+++ routes/route.js
@@ -6,29 +6,60 @@ const product = require("../controller/product.js");
 const productDetails = require("../controller/productDetails.js");
 const getProducts = require("../controller/getProducts.js");
 const getProductDetails = require("../controller/getProductDetails.js");
-const addToCart = require("../controller/addToCart.js");
-const cart = require("../controller/cart.js");
+const {
+  cart,
+  addToCart,
+  clearCart,
+  deleteFromCart,
+  decrementQuantity,
+} = require("../controller/cart.js");
 const getProduct = require("../controller/getProduct.js");
 
 const router = express.Router();
 
+// Route to add a product
+
 router.post("/products", product);
+
+// Route to add product details
 router.post("/productDetails", productDetails);
+// Route to get product details
 router.get("/getProductDetails", getProductDetails);
+
+// Route to get all products
 router.get("/products", getProducts);
+
+// Route to get a specific product with asin
 router.get("/product", getProduct);
 
+// Route to add a product to the cart
 router.post("/add-to-cart", authenticate, addToCart);
+
+// Route to clear the cart
+router.delete("/clear-cart", authenticate, clearCart);
+
+// Route to delete a specific product from the cart
+router.delete("/delete/:asin", authenticate, deleteFromCart);
+
+// Route to increment the quantity of a specific product in the cart
+router.put("/decrement/:asin", authenticate, decrementQuantity);
+
+// Route to get the cart
 router.get("/cart", authenticate, cart);
 
+// Route to create a new user
 router.post("/signup", createUser);
+
+// Route to login
 router.post("/login", login);
+
+// Route to logout
 router.get("/logout", (req, res) => {
   res.clearCookie("token");
   res.json({ message: "Logged out" });
 });
 
-router.get("/profile", authenticate, (req, res) => {
+router.get("/me", authenticate, (req, res) => {
   res.json({
     message: `Welcome ${req.user.name}`,
     user: {
